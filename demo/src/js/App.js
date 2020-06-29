efcf3b92257f125cf9401b0e6bbf03d72d1fdb6e
Dave <discodavey@gmail.com>
2020-06-29 15:05:22 +0100

    Checkbox switch label styling (#53)
    * Adding styling for checkbox labels
    
    * Amend upload button

--- demo/src/js/App.js
+++ demo/src/js/App.js
@@ -19,9 +19,15 @@ document.querySelector('#filePicker').addEventListener('change', (e) => {
 });
 
 document.getElementById('type-of-model').onclick = function(event) {
+  const biorxivModel = document.querySelector('[aria-label="biorxiv"]');
+  const otherModel = document.querySelector('[aria-label="other"]');
   if (!event.target.checked) {
+    biorxivModel.classList.add('selected-model');
+    otherModel.classList.remove('selected-model');
     fileHandler.setConvertApiUrl(BIORXIV_MODEL_CONVERT_API_URL);
   } else {
+    biorxivModel.classList.remove('selected-model');
+    otherModel.classList.add('selected-model');
     fileHandler.setConvertApiUrl(OTHER_MODEL_CONVERT_API_URL);
   }
   document.querySelector('#filePicker').value = null;
